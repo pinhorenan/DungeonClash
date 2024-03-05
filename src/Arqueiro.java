4a4fbe986c8b6a2c98c6cba91e8d2667aed648d5
Gabriel Moura <79274081+gbrimoura@users.noreply.github.com>
2024-03-05 14:24:37 -0300

    so alguns comentarios

--- src/Arqueiro.java
+++ src/Arqueiro.java
@@ -11,7 +11,7 @@ public class Arqueiro extends Classe {
         habilidades.add(new Habilidade("Atirar Flecha", new PesosDeAtributos(0.3, 0.5, 0), 3, false, false));
         habilidades.add(new Habilidade())
     }// SINCERAMENTE QUE ESPECIFICACAO MERDA ESSA PARTE DE BOTAR TIPO PESOS DE ATRIBUTOS COMO REQUISITO P PARAMETRO DE HABILIDADE QUE IMPLEMENTACAO BURRA PQ Q A GENTE EH OBRIGADO A FAZER ASSIM AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
+// sinceramente a gente tambem odiou
     // Adicione métodos específicos do arqueiro, se necessário
 
     // Métodos para as habilidades específicas
