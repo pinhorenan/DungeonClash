2f6584bd1d64d8d67902fff990abc7ee7d72debd
Gabriel Moura <79274081+gbrimoura@users.noreply.github.com>
2024-03-05 14:58:32 -0300

    eu vou coringar

--- src/Habilidade.java
+++ src/Habilidade.java
@@ -1,15 +1,19 @@
 public class Habilidade {
 
     private String nome; // Nome da habilidade
-    private PesosDeAtributos pesosAtributos; // Define pesos de dano e mana(magia) associados a habilidade
+    private PesosDeAtributos pesosDano;
+    private PesosDeAtributos pesosMana; // Define pesos de dano e mana(magia) associados a habilidade
     private int tempo; // Tempo de espera associado a habilidade
     private boolean afetaGrupo; // Se afeta todos os personagens da equipe adversária
     private boolean afetaAmigos; // Se pode ser utilizada em amigos
     private static int ID; // Identificador único da habilidade
 
-    public Habilidade(String nome, PesosDeAtributos pesosAtributos, int tempo, boolean afetaAmigos, boolean afetaGrupo) {
+    public Habilidade(String nome, PesosDeAtributos pesosDano, PesosDeAtributos pesosMana, int tempo, boolean afetaAmigos, boolean afetaGrupo) {
+    // mago 
+        // private Habilidade cura = new Habilidade("Cura", 
         this.nome = nome;
-        this.pesosAtributos = pesosAtributos;
+        this.pesosDano = pesosDano;
+        this.pesosMana = pesosMana;
         this.tempo = tempo;
         this.afetaGrupo = afetaGrupo;
         this.afetaAmigos = afetaAmigos;
