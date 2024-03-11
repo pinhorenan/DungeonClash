ae601cbfc75d8ffaac74a4568a022b856b5e1bc3
Renan Pinho <pinhorenan@icloud.com>
2024-03-11 06:01:44 -0300

    Troca do uso de ArrayList para Sets. Muito a fazer nessa aqui ainda.

--- src/Equipe.java
+++ src/Equipe.java
@@ -1,28 +1,25 @@
+import java.util.Set;
+
 public class Equipe {
-    /*  classe Equipe deve manter uma coleção de personagens (agregação). O projeto básico define que
-    cada batalha se dá entre duas equipes, a equipe do herois e a equipe dos inimigos. Como
-    funcionalidades básicas, esta classe deve implementar métodos para adicionar e buscar personagens
-    em uma equipe, retornar a equipe (coleção), computar pontos de experiência para a equipe,
-    determinar o próximo atacante, atualizar o tempo de espera, entre outros. */
-    private List<Personagem> integrantes;
+
+    private Set<Personagem> equipe;
 
     public Equipe() {
-        this.integrantes = new ArrayList<>();
     }
 
     public void addIntegrante(Personagem integrante) {
-        integrantes.add(integrante);
+        equipe.add(integrante);
     }
 
     public void distribuirPE(int lvlInimigo) {
-        for (Personagem integrante: integrantes) {
+        for (Personagem integrante: grupo) {
             integrante.ganharPE(lvlInimigo);
         }
     }
 
     public Personagem searchID(int ID) {
-        for (Personagem integrante : integrantes) {
-            if (ID == personagem.getID) {
+        for (Personagem integrante : grupo) {
+            if (ID == Personagem.getID()) {
                 return personagem;
             } else {
                 return null;
@@ -31,7 +28,7 @@ public class Equipe {
     }
 
     // getter
-    public List<Personagem> getIntegrantes() {
-        return integrantes;
+    public Set<Personagem> getGrupo() {
+        return grupo;
     }
 }
