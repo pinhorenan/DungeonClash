65559a6786682974926c5e39362412d2b8f8e1fb
Renan Pinho <pinhorenan@icloud.com>
2024-03-12 08:29:30 -0300

    Grande refatoração da de Batalha, descentralização de responsabilidades de certos métodos como os de ataque para a classe Equipe

--- src/Batalha.java
+++ src/Batalha.java
@@ -1,30 +1,64 @@
+import java.util.Random;
+
 public class Batalha {
 
-    // -------------------------------------------- ATRIBUTOS -------------------------------------------- //
-    Equipe herois;
-    Equipe inimigos;
-    int contadorTurnos;
+    private int contadorTurnos;
 
-    // ------------------------------------------ CONSTRUTOR ------------------------------------------- //
-    public Batalha(Equipe herois, Equipe inimigos) {
-        int contadorTurnos = 0;
-        herois.getProximoAtacante();
-        inimigos.getProximoAtacante();
+    // Construtor
+    public Batalha() {
+        this.contadorTurnos = 0;
     }
 
-    // -------------------------------------------- MÉTODOS -------------------------------------------- //
-    public void passarTurno() {
-        contadorTurnos += 1;
+    public void iniciarBatalha(Equipe herois, Equipe inimigos) {
+        exibirInformacoesEquipes(herois, inimigos);
 
-        herois.atualizarEspera();
-        inimigos.atualizarEspera();
+        // Sorteia quem ataca primeiro
+        Personagem primeiroAtacante = sortearPrimeiroAtacante(herois, inimigos);
 
-        for (Personagem inimigo: inimigos.getIntegrantes()) {
-            if (inimigo.getMorto()){
-                int ganhoPE = inimigo.getNivel()*5;
-                herois.distribuirPE(ganhoPE);
-                inimigos.removerIntegrante(inimigo);
-                }
-            }
+        // Inicia os turnos
+        while (herois.peloMenosUmVivo() && inimigos.peloMenosUmVivo()) {
+            realizarTurno(herois, inimigos, primeiroAtacante);
+            exibirInformacoesEquipes(herois, inimigos);
         }
+
+        // Exibe o resultado da batalha
+        exibirResultadoBatalha(herois, inimigos);
     }
+
+    private Personagem sortearPrimeiroAtacante(Equipe herois, Equipe inimigos) {
+        Random random = new Random();
+        return (random.nextBoolean()) ? herois.getProximoAtacante() : inimigos.getProximoAtacante();
+    }
+
+    private void realizarTurno(Equipe herois, Equipe inimigos, Personagem primeiroAtacante) {
+        System.out.println("\n --- Turno " + contadorTurnos + " --- ");
+        System.out.println("É a vez de " + primeiroAtacante.getNome() + " atacar!");
+
+        // Exibe habilidades disponíveis do personagem que vai atacar
+        primeiroAtacante.exibirHabilidades(); // Implementar
+
+        // Implementar lógica pra escolher a habilidade e o alvo.
+
+        // Atualiza o tempo de espera de maneira correspondente a sua habilidade.
+        primeiroAtacante.atualizarTempoEspera(); // Implementar
+
+        // Incrementa o contador de turnos
+        contadorTurnos++;
+    }
+
+    private void exibirInformacoesEquipes(Equipe herois, Equipe inimigos) {
+        System.out.println("\n --- Informações das Equipes ---");
+        System.out.println("\nHeróis: ");
+        herois.exibirInformacoes(); // Implementar
+        System.out.println("\nInimigos: ");
+        inimigos.exibirInformacoes(); // Implementar
+    }
+
+    private void exibirResultadoBatalha(Equipe herois, Equipe inimigos) {
+        // Implementar
+    }
+}
+
+
+
+
