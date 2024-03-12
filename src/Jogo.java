90c8fdd0d8da225c33e41495f7a824be20313f59
Gabriel Moura <79274081+gbrimoura@users.noreply.github.com>
2024-03-12 01:36:43 -0300

    comentarios pra pensar um pouco

--- src/Jogo.java
+++ src/Jogo.java
@@ -13,7 +13,12 @@ public class Jogo {
 // comentário pra lembrar de implementar alguma forma de portar o arquivo .txt e transformar ele em string (espero q seja minimamente parecido com shell script)
 
   public void iniciar() {
-    
+    // ANOTAÇÕES
+    // "INICIAR" DEVE ABRIR UM MENU PRA CRIAÇÃO DE PERSONAGENS
+    // APOS ISSO, DEVE ABRIR O ARQUIVO E TRANSFORMAR SEU CONTEUDO EM FASES
+    // A PRIMEIRA LINHA DA FASE DEVE SER PRINTADA
+    // A SEGUNDA LINHA DEVE SER TRANSFORMADA NA EQUIPE INIMIGA, COM CADA ITEM SENDO UM INTEGRANTE
+    // APOS UMA FASE ACABAR, A PROXIMA DEVE COMEÇAR (FAZER UM SET DE FASES???????????)
   }
 
   public void carregarEquipe() {
