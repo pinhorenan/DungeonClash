4edbf770b7e2fe95b87144e2f9628b2a8fbde945
Renan Pinho <pinhorenan@icloud.com>
2024-03-12 15:08:39 -0300

    Personagens não podem mais morrer, serão apenas atordoados. Revisão da documentação. Revisão nos métodos relacionados às habilidades.

--- src/Personagem.java
+++ src/Personagem.java
@@ -2,17 +2,16 @@ import java.util.Set;
 
 public class Personagem implements Comparable<Personagem> {
 
-    // -------------------------------------------- ATRIBUTOS -------------------------------------------- //
     private final String nome;
     private final Classe classe;
     private static int proximoID = 1;
     private final int ID;
     private int nivel, PE, tempoEspera;
     private float PVmax, PV, PMmax, PM;
-    private boolean morto;
+    private boolean atordoado;
 
-    // ------------------------------------------ CONSTRUTOR ------------------------------------------- //
     public Personagem(String nome, Classe classe) {
+        // Construtor. Deverá passar o nome (String) e uma classe, a classe deverá ser criada no campo de parâmetro
         this.nome = nome;
         this.classe = classe;
         ID = gerarProximoID();
@@ -23,17 +22,17 @@ public class Personagem implements Comparable<Personagem> {
         PMmax = classe.inteligencia + ((float) classe.agilidade / 3);
         PV = PVmax;
         PM = PMmax;
-        morto = false;
+        atordoado = false;
     }
-
-    // -------------------------------------------- MÉTODOS -------------------------------------------- //
 
     @Override
     public int compareTo(Personagem outro) {
+        // Define um método de comparação entre instâncias de Personagens para comparar seus tempoEspera. Chamado para definir o próximoAtacante.
         return Integer.compare(this.getTempoEspera(), outro.getTempoEspera());
     }
 
     public void subirNivel() {
+        // Incrementa o nível do Personagem que chamar. Também irá "aumentar o nível" da instância de classe desse Personagem. Os PE são zerados após a subida de nível e qualquer excesso é desperdiçado.
         if (PE >= (nivel * 25)) {
             nivel++;
             classe.subirNivel();
@@ -44,156 +43,167 @@ public class Personagem implements Comparable<Personagem> {
     }
 
     public void atualizarTempoEspera(int tempo) {
+        // Define o tempo de espera do Personagem que chamar para "tempo". Chamado após usar habilidades.
         this.setTempoEspera(tempo);
     }
 
     private static int gerarProximoID() {
+        // Gera um novo ID incrementando o último ID registrado. Chamado durante instanciação de Personagens.
         return proximoID++;
     }
 
     public void sofrerDano(int dano) {
+        // Subtrai o PV do Personagem que chamar pelo int Dano. Chamado durante uso de habilidades.
         if (PV > 0) {
             PV -= dano;
         }
     }
 
     public int custoMana(Habilidade habilidade) {
+        // Retorna o custo em PE ao usar uma habilidade dependendo do nível do personagem que chamar.
         return (int) (nivel * habilidade.calcularCustoMana(classe));
     }
 
     public int danoCausado(Habilidade habilidade) {
+        // Retorna o dano em PV ao usar uma habilidade dependendo do nível do personagem que chamar.
         return (int) (nivel * habilidade.calcularDanoCausado(classe));
     }
 
     public void usarHabilidade(Habilidade habilidade, Personagem alvo) {
-        if (morto) {
-            System.out.println("Você está morto.");
-        } else
-            if(tempoEspera != 0) {
-                System.out.println("Você não pode atacar ainda.");
-        } else
-            if(custoMana(habilidade) > PM) {
-                System.out.println("Você não tem mana.");
-        } else
-            if(alvo.morto) {
-                System.out.println(alvo.nome + " já está morto.");
-        } else
-            if(!classe.getHabilidades().contains(habilidade)) {
-                System.out.println("Você não possui essa habilidade");
-        } else {
-                if(habilidade.getIsAfetaAmigos()){
-                    alvo.setPV(alvo.getPV()+danoCausado(habilidade));
-                } else if (!habilidade.getIsAfetaAmigos()) {
-                    alvo.sofrerDano(danoCausado(habilidade));
-                }
-                    setTempoEspera(habilidade.getTempoEspera());
-                    setPM(this.getPM() - custoMana(habilidade));
-                    System.out.println("Você utiliza "+ habilidade.getNome() + " contra " + alvo.getNome() + ", causando " + danoCausado(habilidade) + " de dano!");
-
-                if (alvo.PV==0) {
-                    alvo.morrer();
-                    System.out.println("Você MATOU VIOLENTAMENTE o " + alvo.getNome() + "! Sinta-se orgulhoso!");
-                }
-            }
-    }
-
-    public void usarHabilidade(Habilidade habilidade, Equipe grupoAlvo) {
-        if(tempoEspera != 0) {
-                System.out.println("Você não pode atacar ainda!");
-        } else if(habilidade.calcularCustoMana(classe) > PM) {
-                System.out.println("Você não tem mana.");
-        } else if(!classe.getHabilidades().contains(habilidade)) {
-                System.out.println("Você não possui essa habilidade!");
-        } else if(!habilidade.getIsAfetaGrupo()) {
-                System.out.println("Essa habilidade não pode ser usada contra um grupo!");
-        } else {
-                Set<Personagem> integrantes = grupoAlvo.getIntegrantes();
-                for(Personagem personagem: integrantes) {
-                    personagem.sofrerDano(danoCausado(habilidade));
-                    if (personagem.getPV() == 0){
-                        personagem.setMorto();
-                    }
-                }
+        // Método para usar a Habilidade. Primeiro irá verificar se quem chamou cumpre os requisitos e então aplica os efeitos no Alvo.
+        if (verificarRestricoes(habilidade, alvo)&&verificarRestricoes(habilidade)) {
+            aplicarEfeitoHabilidade(habilidade, alvo);
         }
     }
 
-    public void exibirHabilidades() {
-        System.out.println("Habilidades disponíveis para " + nome + ":");
+    public void usarHabilidade(Habilidade habilidade, Equipe grupoAlvo) {
+        // Método para usar a Habilidade. Primeiro irá verificar se quem chamou cumpre os requisitos e então aplica os efeitos no grupoAlvo.
+        if (verificarRestricoes(habilidade)) {
+            aplicarEfeitoHabilidade(habilidade, grupoAlvo);
+        }
+    }
 
-        for (Habilidade habilidade : classe.getHabilidades()) {
-            System.out.println(habilidade.getNome());
+    private boolean verificarRestricoes(Habilidade habilidade) {
+        // Método auxiliar para verificar possibilidade de "usarHabilidade()". Restrições sobre quem chama a Habilidade.
+        if (atordoado) {
+            return false;
+        }
+        if (tempoEspera != 0) {
+            return false;
+        }
+        if (habilidade.calcularCustoMana(classe) > PM) {
+            return false;
+        }
+        return classe.getHabilidades().contains(habilidade);
+    }
+
+    private boolean verificarRestricoes(Habilidade habilidade, Personagem alvo) {
+        // Método auxiliar para verificar possibilidade de "usarHabilidade()". Restrições sobre o alvo da habilidade.
+        if (alvo.atordoado) {
+            return false;
+        }
+        return verificarRestricoes(habilidade);
+    }
+
+    private void aplicarEfeitoHabilidade(Habilidade habilidade, Personagem alvo) {
+        // Método que aplica os efeitos de uma habilidade usada, é chamado por "usarHabilidade()"; Versão para ataque em Personagens.
+        if (habilidade.getIsAfetaAmigos()) {
+            alvo.setPV(alvo.getPV() + danoCausado(habilidade));
+        } else {
+            alvo.sofrerDano(danoCausado(habilidade));
+        }
+        setPM(this.getPM() - custoMana(habilidade));
+        if (alvo.PV == 0) {
+            alvo.atordoar();
+        }
+    }
+
+    private void aplicarEfeitoHabilidade(Habilidade habilidade, Equipe grupoAlvo) {
+        // Método que aplica os efeitos de uma habilidade usada, é chamado por "usarHabilidade()"; Versão para ataque em Equipes.
+        Set<Personagem> integrantes = grupoAlvo.getIntegrantes();
+        for (Personagem personagem : integrantes) {
+            personagem.sofrerDano(danoCausado(habilidade));
         }
     }
 
     public void ganharPE(int PE) {
+        // Incrementa Pontos de Experiência ao Personagem.
         this.PE += PE;
     }
 
-    public void morrer() {
-        setMorto();
+    public void atordoar() {
+        // Define o Personagem como Atordoado.
+        setAtordoado(true);
     }
 
-    // -------------------------------------------- GETTERS -------------------------------------------- //
-
     public String getNome() {
+        // Retorna o nome do Personagem.
         return nome;
     }
 
     public Classe getClasse() {
+        // Retorna a instância de classe do Personagem.
         return classe;
     }
 
     public int getNivel() {
+        // Retorna o nível do Personagem.
         return nivel;
     }
 
     public int getPE() {
+        // Retorna os Pontos de Experiência do Personagem.
         return PE;
-        }
+    }
 
     public int getID() {
+        // Retorna o ID do Personagem.
         return ID;
     }
 
     public int getTempoEspera() {
+        // Retorna o Tempo de Espera do Personagem.
         return tempoEspera;
     }
 
     public float getPV() {
+        // Retorna os Pontos de vida do Personagem.
         return PV;
     }
 
     public float getPM() {
+        // Retorna os Pontos de Magia do Personagem.
         return PM;
     }
 
-    public boolean getMorto() {
-        return morto;
+    public boolean getAtordoado() {
+        // Retorna o status de atordoamento do Personagem.
+        return atordoado;
     }
-    // -------------------------------------------- SETTERS -------------------------------------------- //
 
     public void setTempoEspera(int tempoEspera) {
+        // Altera o Tempo de Espera do Personagem.
         this.tempoEspera = tempoEspera;
     }
 
     public void setPE(int PE) {
+        // Altera os Pontos de Experiência do Personagem.
         this.PE = PE;
     }
 
     public void setPV(float PV) {
+        // Altera os Pontos de Vida do Personagem.
         this.PV = PV;
     }
 
     public void setPM(float PM) {
+        // Altera os Pontos de Magia do Personagem.
         this.PM = PM;
     }
 
-    public void setVivo() {
-        morto = false;
-    }
-
-    public void setMorto() {
-        morto = true;
+    public void setAtordoado(boolean atordoado) {
+        // Altera o estado de Atordoamento do Personagem.
+        this.atordoado = atordoado;
     }
 }
 
