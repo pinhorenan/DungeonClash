1ec67d98da020378581b778c5511438b6a238548
Gabriel Moura <79274081+gbrimoura@users.noreply.github.com>
2024-03-12 05:12:13 -0300

    Criação da party em iniciar();

--- src/Jogo.java
+++ src/Jogo.java
@@ -19,7 +19,29 @@ public class Jogo {
   public void iniciar() {
     // ANOTAÇÕES
     // "INICIAR" DEVE ABRIR UM MENU PRA CRIAÇÃO DE PERSONAGENS
+    for (int i = 1; i <=3; ++i){
+      System.out.println("\nNome do " + i + "º Herói ou Heroína: ");
+      String nomeHeroi = scanner.nextLine();
+      System.out.println("\nQual será a classe de " + nomeHeroi + "?");
+      System.out.println("\n1- GUERREIRO\n2- ARQUEIRO\n3- MAGO");
+      int escolhaClasse = scanner.nextLine();
+      switch (escolhaClasse) {
+        case 1:
+            Personagem novoHeroi = new Personagem(nomeHeroi, new Guerreiro());
+        case 2:
+            Personagem novoHeroi = new Personagem(nomeHeroi, new Arqueiro());
+        case 3:
+            Personagem novoHeroi = new Personagem(nomeHeroi, new Mago());
+        default:
+            Personagem novoHeroi = new Personagem(nomeHeroi, new Guerreiro());
+      }
+
+      herois.adicionarIntegrante(novoHeroi);
+    }
+    
     // APOS ISSO, DEVE ABRIR O ARQUIVO E TRANSFORMAR SEU CONTEUDO EM FASES
+
+    
     // A PRIMEIRA LINHA DA FASE DEVE SER PRINTADA
     // A SEGUNDA LINHA DEVE SER TRANSFORMADA NA EQUIPE INIMIGA, COM CADA ITEM SENDO UM INTEGRANTE
     // APOS UMA FASE ACABAR, A PROXIMA DEVE COMEÇAR (FAZER UM SET DE FASES???????????)
