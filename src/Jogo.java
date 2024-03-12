afe6097ab9362d3d59fd6a9a48c1275db2c42c1e
Gabriel Moura <79274081+gbrimoura@users.noreply.github.com>
2024-03-12 01:28:15 -0300

    hey papito
    eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu adoro java eu

--- src/Jogo.java
+++ src/Jogo.java
@@ -2,10 +2,12 @@ public class Jogo {
 
   private Equipe herois;
   private Equipe inimigos;
+  private File arquivo;
 
-  public Jogo() {
+  public Jogo(File arquivo) {
     this.herois = new Equipe();
     this.inimigos = new Equipe();
+    this.arquivo = new arquivo;
   }
 
 // comentário pra lembrar de implementar alguma forma de portar o arquivo .txt e transformar ele em string (espero q seja minimamente parecido com shell script)
