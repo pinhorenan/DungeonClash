ae97013f06d3055dcf0ef2c391a6e519d01cee01
Renan Pinho <pinhorenan@icloud.com>
2024-03-13 17:42:18 -0300

    Removido parâmetro inútil em "buscarIntegrante()" (que por si só é uma função, até então, inútil, mas enfim...).

--- src/Equipe.java
+++ src/Equipe.java
@@ -8,13 +8,13 @@ public class Equipe {
     private final boolean isInimigos;
 
     public Equipe(boolean isInimigos) {
-        // Construtor.
+            // Construtor.
         this.isInimigos = isInimigos;
         integrantes = new HashSet<>();
     }
 
     public void adicionarIntegrante(Personagem personagem) {
-        // Adiciona novos Personagens ao conjunto Equipe.
+            // Adiciona novos Personagens ao conjunto Equipe.
         if(getIsInimigos()) {
             integrantes.add(personagem);
         } else if (integrantes.size() < 3){
@@ -23,15 +23,15 @@ public class Equipe {
     }
 
     public void removerIntegrante(Personagem integrante) {
-        // Remove personagens do conjunto Equipe.
+            // Remove personagens do conjunto Equipe.
         if (!integrantes.contains(integrante)) {
             return;
         }
         integrantes.remove(integrante);
     }
 
-    public Personagem buscarIntegrante(Equipe equipe, int ID) {
-        // Busca um Personagem integrante de equipe a partir de seu ID e retorna-o.
+    public Personagem buscarIntegrante(int ID) {
+            // Busca um Personagem integrante de equipe a partir de seu ID e retorna-o.
         for (Personagem personagem : integrantes) {
             if (personagem.getID() == ID) {
                 return personagem;
@@ -40,7 +40,7 @@ public class Equipe {
     }
 
     public void distribuirPE(int ganhoPE) {
-        // Incrementa PE à todos integrantes da Equipe e faz realiza a checa se algum integrante deve subir de nível.
+            // Incrementa PE à todos integrantes da Equipe e faz realiza a checa se algum integrante deve subir de nível.
         for (Personagem personagem: integrantes) {
             personagem.ganharPE(ganhoPE);
             personagem.subirNivel();
@@ -48,7 +48,7 @@ public class Equipe {
     }
 
     public Personagem definirProximoAtacante() {
-        // Compara os tempoEspera entre integrantes para retornar o com o menor valor, que será o próximo atacante. Em caso de valores iguais a escolha é feita aleatóriamente.
+            // Compara os tempoEspera entre integrantes para retornar o com o menor valor, que será o próximo atacante. Em caso de valores iguais a escolha é feita aleatóriamente.
         if (integrantes.isEmpty()) {
             return null; // Luis, você pode tentar tratar de outra forma as exceções, já que você manja.
         }
@@ -56,7 +56,7 @@ public class Equipe {
     }
 
     public boolean peloMenosUmVivo() {
-        // Verifica se há pelo menos um integrante não atordoado na Equipe. Quando todos integrantes de uma equipe forem atordoados uma batalha irá chegar ao seu fim.
+            // Verifica se há pelo menos um integrante não atordoado na Equipe. Quando todos integrantes de uma equipe forem atordoados uma batalha irá chegar ao seu fim.
         for (Personagem integrante : integrantes) {
             if (!integrante.getAtordoado()) {
                 return true;
@@ -66,17 +66,17 @@ public class Equipe {
     }
 
     public Set<Personagem> getIntegrantes() {
-        // Retorna o conjunto de integrantes da Equipe.
+            // Retorna o conjunto de integrantes da Equipe.
         return integrantes;
     }
 
     public boolean getIsInimigos() {
-        // Retorna um valor true ou false para IsInimigo. Quando IsInimigo == True as equipes podem ter um número indeterminado de integrantes.
+            // Retorna um valor true ou false para IsInimigo. Quando IsInimigo == True as equipes podem ter um número indeterminado de integrantes.
         return isInimigos;
     }
 
     public void decrementaTempoEspera(){
-        // Decrementa o tempo de espera de cada personagem da equipe. Será chamada pelas duas equipes no final de cada turno.
+            // Decrementa o tempo de espera de cada personagem da equipe. Será chamada pelas duas equipes no final de cada turno.
         for(Personagem personagem : integrantes) {
             if (personagem.getTempoEspera() >0 ) {
                 personagem.setTempoEspera(personagem.getTempoEspera() - 1);
