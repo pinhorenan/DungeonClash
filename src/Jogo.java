96f444a52b9c14a108dd54f56381c9d71f448ba9
Luis Eduardo <123844018+FnthoN@users.noreply.github.com>
2024-03-13 00:10:26 -0300

    Update Jogo.java

--- src/Jogo.java
+++ src/Jogo.java
@@ -38,14 +38,14 @@ public class Jogo {
 
     // História
     String procurarPor = "fase";
-    boolean verificacao;
+    boolean verificacao = false;
     for (String linha : linhasArquivo){
         if (!(linha.toLowerCase().contains(procurarPor.toLowerCase()))){
           carregarInimigos(linha);
           verificacao = true;
         } else {
           System.out.println("\n" + linha.substring(5));
-          if (verificacao == true){
+          if (verificacao){
             iniciarBatalha();
             verificacao = false;
           }
