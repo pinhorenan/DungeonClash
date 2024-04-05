6526ff861283860170c6e037aefbf3360e19be1e
Renan Pinho <pinhorenan@icloud.com>
2024-04-05 04:54:46 -0300

    Prototype for file reading util class.

---
+++ src/main/java/com/dungeonclash/utils/files/ReadFile.java
@@ -0,0 +1,23 @@
+package main.java.com.dungeonclash.utils.files;
+
+public class ReadFile {
+
+    public void readEnemies() {
+        // Read enemie from file
+    }
+
+    public void readBattle() {
+        readEnemies();
+        // Read battle from file
+    }
+
+    public void readChapter() {
+        readBattle();
+        // Read chapter from file
+    }
+
+
+}
+
+
+
